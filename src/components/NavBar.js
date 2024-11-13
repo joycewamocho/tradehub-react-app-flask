0e44fbd1a4ae3d198536632d92b1bd1bf6be83a7
joyce wamocho <joywamo@gmail.com>
2024-11-13 08:24:35 +0300

    styled navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,51 @@
-import { Link } from "react-router-dom"
-function NavBar(){
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">TradeHub</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/products">Products</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/cart">Cart</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Login</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    )
+import { Link } from "react-router-dom";
+import "./NavBar.css"; // Add custom CSS styles
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
+            <div className="container-fluid">
+                <Link className="navbar-brand fw-bold fs-3" to="/home">
+                    <span className="text-warning">Trade</span>Hub
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/home">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/products">
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                Cart
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
