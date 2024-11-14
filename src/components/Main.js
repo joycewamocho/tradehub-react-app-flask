3c3ecda6deb7ff7ec345450873e4e3cf67573871
joyce wamocho <joywamo@gmail.com>
2024-11-14 17:33:59 +0300

    intial commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,38 @@
-import React from "react"
-import ProductList from "./ProductList"
+import React from "react";
+import ProductList from "./ProductList";
+import Search from "./Search";
 
-function Main(){
-    const[products,setProducts]=React.useState([])
+function Main() {
+  const [products, setProducts] = React.useState([]);
+  const [search, setSearch] = React.useState("");
 
-    React.useEffect(()=>{
-        fetch("https://my-json-server.typicode.com/joycewamocho/tradehub-react-app/products")
-        .then((res)=>res.json())
-        .then((data)=>setProducts(data))
-    },[])
+  // Fetch products from API
+  React.useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
+  // Filter products based on search query
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
 
-    return(
-        <div>
-            <ProductList products={products}/>
+  return (
+    <main className="container mt-5">
+      <div className="d-flex flex-column align-items-center">
+        {/* Search Input */}
+        <div className="w-75 mb-4">
+          <Search search={search} setSearch={setSearch} />
         </div>
-    );
+
+        {/* Product List */}
+        <div className="w-100">
+          <ProductList products={filteredProducts} />
+        </div>
+      </div>
+    </main>
+  );
 }
-export default Main
+
+export default Main;
