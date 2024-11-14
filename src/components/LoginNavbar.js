7c7d467c1fc6f2a8f139c0745d03c9e7b09bd1c7
joyce wamocho <joywamo@gmail.com>
2024-11-14 17:32:13 +0300

    creates login navbar

---
+++ src/components/LoginNavbar.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css"; // Add custom CSS styles
+
+function LoginNavbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
+            <div className="container-fluid">
+                <Link className="navbar-brand fw-bold fs-3" to="/">
+                    <span className="text-warning">Trade</span>Hub
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                       
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default LoginNavbar;
